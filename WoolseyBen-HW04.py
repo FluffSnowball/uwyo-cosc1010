1b5ac37d55ec03de954bb60a16253d1252be555b
FluffSnowball <bwoolse1@uwyo.edu>
2024-11-11 21:13:15 +0000

    baby Steps

--- WoolseyBen-HW04.py
+++ WoolseyBen-HW04.py
@@ -1,1 +1,21 @@
+# Ben Woolsey
+# Lab Section: 11
+# Submition Date: 11/11/2024
+# Worked with: Cole Jordan
+from pathlib import Path
 
+path = Path('prompt.txt')
+contents = path.read_text()
+lines = contents.splitlines()
+outputCurrentLine = ""
+
+def wordProcess(input:str):
+    """"""
+
+for i in lines:
+    currentLine = i.split('\t')
+    del currentLine[-1]
+    for x in currentLine:
+        if x.startswith('w'):
+
+    print(currentLine)
