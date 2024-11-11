6394f1c0fcb62bef381daf9083252a7205eae775
FluffSnowball <bwoolse1@uwyo.edu>
2024-11-11 21:26:32 +0000

    Done

--- WoolseyBen-HW04.py
+++ WoolseyBen-HW04.py
@@ -5,17 +5,25 @@
 from pathlib import Path
 
 path = Path('prompt.txt')
+pathOut = Path('out.txt')
+
 contents = path.read_text()
 lines = contents.splitlines()
 outputCurrentLine = ""
 
 def wordProcess(input:str):
-    """"""
+    """Gets the numbers of white space and asterisks"""
+    return int(input[2:])
 
 for i in lines:
     currentLine = i.split('\t')
     del currentLine[-1]
     for x in currentLine:
+        outputCount = wordProcess(x)
         if x.startswith('w'):
+            outputCurrentLine += (" " * outputCount)
+        else: 
+            outputCurrentLine += ("*" * outputCount)
+    outputCurrentLine += '\n'
 
-    print(currentLine)
+    pathOut.write_text(outputCurrentLine)
